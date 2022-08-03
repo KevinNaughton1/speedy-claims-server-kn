97ee3decc112043d4ea04c3bd48159f8acc3c9a3
kevinnaughton1 <kev1naughton@yahoo.ie>
2022-08-03 12:48:27 +0000

    First commit

---
+++ src/main/java/com/allstate/speedyclaimsserverkn/SpeedyClaimsServerKnApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.speedyclaimsserverkn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedyClaimsServerKnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedyClaimsServerKnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allstate/speedyclaimsserverkn/SpeedyClaimsServerKnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allstate.speedyclaimsserverkn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedyClaimsServerKnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
